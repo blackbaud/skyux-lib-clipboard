82ede4baddce3c61d68d356e5fdfbae0961850fd
John Lyons <John.Lyons@blackbaud.me>
2020-09-21 00:05:38 -0400

    JSDocs for copy to clipboard

---
+++ src/app/docs/clipboard/clipboard-docs.component.ts
@@ -0,0 +1,9 @@
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-clipboard-docs',
+  templateUrl: './clipboard-docs.component.html'
+})
+export class ClipboardDocsComponent { }

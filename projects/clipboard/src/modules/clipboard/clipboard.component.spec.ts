e70e4ff5c034fa0cbb78f547850972d5e83d126e
Trevor Burch <Trevor.Burch@blackbaud.com>
2022-03-23 02:16:06 -0400

    Add ability to configure the aria-label and aria-labelledby attributes

--- projects/clipboard/src/modules/clipboard/clipboard.component.spec.ts
+++ projects/clipboard/src/modules/clipboard/clipboard.component.spec.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/core/testing';
 
 import {
-  expect
+  expect, expectAsync
 } from '@skyux-sdk/testing';
 
 import {
@@ -25,6 +25,7 @@ import {
 import {
   SkyClipboardModule
 } from './clipboard.module';
+import { SkyClipboardTestComponent } from './fixtures/clipboard.component.fixture';
 
 class MockClipboardService {
   public copyContent(element: HTMLElement) { }
@@ -32,8 +33,12 @@ class MockClipboardService {
 }
 
 describe('SkyCopyToClipboardComponent', () => {
-  let component: SkyCopyToClipboardComponent;
-  let fixture: ComponentFixture<SkyCopyToClipboardComponent>;
+
+  function getCopyToClipboardButton(fixture: ComponentFixture<SkyClipboardTestComponent>): HTMLElement {
+    return fixture.nativeElement.querySelector('button');
+  }
+  let component: SkyClipboardTestComponent;
+  let fixture: ComponentFixture<SkyClipboardTestComponent>;
   let element: HTMLElement;
   let mockTestElement: any;
   let mockClipboardService: any;
@@ -42,6 +47,9 @@ describe('SkyCopyToClipboardComponent', () => {
     mockClipboardService = new MockClipboardService();
 
     TestBed.configureTestingModule({
+      declarations: [
+        SkyClipboardTestComponent
+      ],
       imports: [
         SkyClipboardModule
       ],
@@ -51,50 +59,115 @@ describe('SkyCopyToClipboardComponent', () => {
       ]
     });
 
-    fixture = TestBed.createComponent(SkyCopyToClipboardComponent);
+    fixture = TestBed.createComponent(SkyClipboardTestComponent);
     component = fixture.componentInstance;
     element = fixture.nativeElement;
+    fixture.detectChanges();
   });
 
   it('should call the clipboard service with the copyTarget element', fakeAsync(() => {
     spyOn(mockClipboardService, 'copyContent').and.callThrough();
-    mockTestElement = document.createElement('div');
-    component.copyTarget = mockTestElement;
     fixture.detectChanges();
-    component.copyToClipboard();
+    const target = fixture.nativeElement.querySelector('#test-copy-target');
+    getCopyToClipboardButton(fixture).click();
+    fixture.detectChanges();
     tick(1000);
-    expect(mockClipboardService.copyContent).toHaveBeenCalledWith(mockTestElement);
+    expect(mockClipboardService.copyContent).toHaveBeenCalledWith(target);
   }));
 
   it('should set the value of buttonActive to true for 1 second after click', fakeAsync(() => {
-    expect(component.buttonActive).toBe(false);
-    component.copyToClipboard();
-    expect(component.buttonActive).toBe(true);
+    expect(component.copyToClipboardComponent.buttonActive).toBe(false);
+    component.copyToClipboardComponent.copyToClipboard();
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
     tick(1000);
-    expect(component.buttonActive).toBe(false);
+    expect(component.copyToClipboardComponent.buttonActive).toBe(false);
   }));
 
   it('should reset the timeout if clicked again before timeout expires', fakeAsync(() => {
-    expect(component.buttonActive).toBe(false);
-    component.copyToClipboard();
-    expect(component.buttonActive).toBe(true);
+    expect(component.copyToClipboardComponent.buttonActive).toBe(false);
+    component.copyToClipboardComponent.copyToClipboard();
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
     tick(500);
-    expect(component.buttonActive).toBe(true);
-    component.copyToClipboard();
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
+    component.copyToClipboardComponent.copyToClipboard();
     tick(500);
-    expect(component.buttonActive).toBe(true);
-    component.copyToClipboard();
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
+    component.copyToClipboardComponent.copyToClipboard();
     tick(500);
-    expect(component.buttonActive).toBe(true);
-    component.copyToClipboard();
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
+    component.copyToClipboardComponent.copyToClipboard();
     tick(500);
-    expect(component.buttonActive).toBe(true);
+    expect(component.copyToClipboardComponent.buttonActive).toBe(true);
     tick(500);
-    expect(component.buttonActive).toBe(false);
+    expect(component.copyToClipboardComponent.buttonActive).toBe(false);
   }));
 
-  it('should pass accessibility', async(() => {
+  it('should display the given button text when not clicked', () => {
+    expect(getCopyToClipboardButton(fixture).innerText.trim()).toEqual('COPY');
+  });
+
+  it('should display the given button clicked text after the button is clicked', () => {
+    const button = getCopyToClipboardButton(fixture);
+    button.click();
     fixture.detectChanges();
-    expect(element).toBeAccessible();
+    expect(button.innerText.trim()).toEqual('COPIED');
+  });
+
+  it('should should reset the button text after a second', fakeAsync(() => {
+    const button = getCopyToClipboardButton(fixture);
+    button.click();
+    fixture.detectChanges();
+    expect(button.innerText.trim()).toEqual('COPIED');
+    fixture.detectChanges();
+    tick(1000);
+    fixture.detectChanges();
+    expect(button.innerText.trim()).toEqual('COPY');
   }));
+
+  it('should specify a default aria-label when none is given', () => {
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('aria-label')).toEqual('Copy to clipboard');
+    expect(button.getAttribute('aria-labelledby')).toBeNull();
+  });
+
+  it('should specify an aria-label when one is given', () => {
+    component.ariaLabel = 'TEST-LABEL';
+    fixture.detectChanges();
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('aria-label')).toEqual('TEST-LABEL');
+    expect(button.getAttribute('aria-labelledby')).toBeNull();
+  });
+
+  it('should specify an aria-labelledby attribute when one is given', () => {
+    component.ariaLabelledBy = 'test-aria-labelledby';
+    fixture.detectChanges();
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('aria-label')).toBeNull();
+    expect(button.getAttribute('aria-labelledby')).toEqual('test-aria-labelledby');
+  });
+
+  it('should specify an aria-labelledby attribute when one is given and an aria-label is given', () => {
+    component.ariaLabel = 'TEST-LABEL';
+    component.ariaLabelledBy = 'test-aria-labelledby';
+    fixture.detectChanges();
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('aria-label')).toBeNull();
+    expect(button.getAttribute('aria-labelledby')).toEqual('test-aria-labelledby');
+  });
+
+  it('should specify a default title when none is given', () => {
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('title')).toEqual('Copy to clipboard');
+  });
+
+  it('should specify a title when one is given', () => {
+    component.title = 'TEST-TITLE';
+    fixture.detectChanges();
+    const button = getCopyToClipboardButton(fixture);
+    expect(button.getAttribute('title')).toEqual('TEST-TITLE');
+  });
+
+  it('should pass accessibility', async () => {
+    await expectAsync(element).toBeAccessible();
+  });
 });
--- projects/clipboard/src/modules/clipboard/clipboard.component.ts
+++ projects/clipboard/src/modules/clipboard/clipboard.component.ts
@@ -17,14 +17,45 @@ import {
   templateUrl: './clipboard.component.html'
 })
 export class SkyCopyToClipboardComponent {
+  /**
+   * Specifies an ARIA label for the copy to clipboard button
+   * [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility).
+   * If the button includes a visible label outside of the button itself, use `ariaLabelledBy` instead.
+   */
+  @Input()
+  public ariaLabel: string;
+
+  /**
+   * Specifies the HTML element ID (without the leading #) of the element that labels the copy to clipboard button. This sets the input's aria-labelledby attribute
+   * [to support accessibility](https://developer.blackbaud.com/skyux/learn/accessibility).
+   * If the button does not include a visible label outside of the button itself, use `ariaLabel` instead.
+   */
+   @Input()
+   public ariaLabelledBy: string;
+
+  /**
+   * Specifies the HTMLElement which contains the content being copied.
+   */
   @Input()
   public copyTarget: HTMLElement;
 
+  /**
+   * Specifies the label for the copy to clipboard button when the button has not been clicked.
+   */
   @Input()
   public buttonText: string;
 
+  /**
+   * Specifies the label for the copy to clipboard button after the button has been clicked.
+   */
   @Input()
   public buttonClickedText: string;
+
+  /**
+   * Specifies a title attribute for the copy to clipboard button.
+   */
+  @Input()
+  public title: string;
 
   public buttonActive: boolean = false;
   private timeout: any;
